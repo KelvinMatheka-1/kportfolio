20fa6025001c05beab5be6db2e43e75e1d1e5ede
KelvinMatheka-1 <beingmatheka@gmail.com>
2026-08-03 12:20:11 +0300

    art

--- components/Misc/AnimatedBackground.tsx
+++ components/Misc/AnimatedBackground.tsx
@@ -1,21 +1,5 @@
 import { memo } from 'react'
-import { Box } from '@chakra-ui/react'
-import StarSparkles from 'components/Misc/StarSparkles'
 
-const AnimatedBackground = () => (
-  <Box
-    position="fixed"
-    top={0}
-    left={0}
-    right={0}
-    bottom={0}
-    overflow="hidden"
-    zIndex={-1}
-    pointerEvents="none"
-    aria-hidden="true"
-  >
-    <StarSparkles />
-  </Box>
-)
+const AnimatedBackground = () => null
 
 export default memo(AnimatedBackground)
